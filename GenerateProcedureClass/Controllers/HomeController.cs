7a9a17cbbb9d12f41d88100a2e63ec80605a6755
Tuhin <tuhin.iiuc2012@gmail.com>
2020-09-07 14:41:47 +0600

    Add project files.

---
+++ GenerateProcedureClass/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using ADO.NET_Store_Procedure;
+using GenerateProcedureClass.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenerateProcedureClass.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Data.ApplicationDbContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private string connectionstring;
+
+        public HomeController(ApplicationDbContext context, IHostingEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+            connectionstring = _context.Database.GetDbConnection().ConnectionString;
+        }
+
+        public IActionResult Index()
+        {
+            List<string[]> paraStrings = new List<string[]>();
+            paraStrings.Add(new[] { "@id", "1" });
+            paraStrings.Add(new[] { "@name", "1" });
+            List<sp_donate> list = SpProcedureCallBack.GetStoreProcedureCallBack<sp_donate>(connectionstring, "sp_donate", paraStrings);
+
+
+            return View();
+        }
+
+        public IActionResult GenerateFile()
+        {
+            string contentRootPath = _hostingEnvironment.ContentRootPath + "\\Models";
+
+            // string generateAllProcedureClass = SpProcedureCallBack.GenerateAllProcedureClass(connectionstring, contentRootPath);
+            {
+                List<string[]> paraStrings = new List<string[]>();
+                paraStrings.Add(new[] { "@id", "1" });
+                paraStrings.Add(new[] { "@name", "1" });
+
+                string singleFileWithParamter =
+                    SpProcedureCallBack.GenerateSingleProcedureClass(connectionstring, "sp_donate", contentRootPath,
+                        paraStrings);
+
+
+                //string singleFileWithoutParameter = SpProcedureCallBack.GenerateSingleProcedureClass(connectionstring, "sp_customer", contentRootPath, null);
+
+
+                return Content(singleFileWithParamter);
+
+            }
+        }
+    }
+}
+
---
+++ GenerateProcedureClass/Models/sp_donate.cs
@@ -0,0 +1,21 @@
+using System;
+public class sp_donate
+{
+	public System.Int32 Id { get; set; }
+	public System.Double Amount { get; set; }
+	public System.Int32 GiftType { get; set; }
+	public System.String Title { get; set; }
+	public System.String Name { get; set; }
+	public System.String Country { get; set; }
+	public System.Int32 PhoneNumber { get; set; }
+	public System.String EmailAddress { get; set; }
+	public System.String Description { get; set; }
+	public System.String Address { get; set; }
+	public System.String TransactionId { get; set; }
+	public System.Boolean Recognition { get; set; }
+	public System.String StudentId { get; set; }
+	public System.Boolean PaymentStatus { get; set; }
+	public System.String Currency { get; set; }
+	public System.DateTime PayDate { get; set; }
+
+}
